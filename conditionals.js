8ebccd4f7412c649b92a02bd45ef930ff3de0473
Babison Vilvarajah <officialbookingssf@gmail.com>
2020-12-24 17:40:03 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -11,7 +11,7 @@ if (raining) {
 
 console.log("Now you're ready to go outside!");
 */
-const temperature = -12;
+/* const temperature = -12;
 
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
@@ -22,3 +22,21 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+*/
+/*
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+*/
+/*const temperature = -41
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+*/
+const raining = false;
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
